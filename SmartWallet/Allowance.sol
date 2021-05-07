775d394b9901f24434106cb84267a907778a3ebe
robetalamin <81731377+robetalamin@users.noreply.github.com>
2021-05-07 14:28:46 +0700

    Create Allowance.sol

---
+++ SmartWallet/Allowance.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.8.00;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
+
+contract Allowance {
+    
+    using SafeMath for uint;
+    
+    address public owner;
+    
+    constructor () {
+        owner = msg.sender;
+    }
+    
+    modifier onlyowner() {
+        require(msg.sender == owner, "you are not allowed");
+        _;
+    }
+    
+    event AllowanceChanged(address indexed _ForWho, address indexed _By, uint _OldAmount, uint _NewAmount);
+    
+    mapping(address => uint) public allowance;
+    
+    function SetAllowance(address _who, uint _amount) public onlyowner {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
+        allowance[_who] = _amount;
+    }
+    
+    modifier onlyowner1(uint _amount) {
+        require(msg.sender == owner || allowance[msg.sender] >= _amount, "you are not allowed");
+        _;
+    }
+    
+    function RedAllowance(address _who, uint _amount) internal {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who].sub(_amount));
+        allowance[_who] = allowance[_who].sub(_amount);
+    }
+}
