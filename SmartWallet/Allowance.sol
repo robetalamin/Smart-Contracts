c94c5ebf4b89e10f854851e13dab55d45075d237
robetalamin <81731377+robetalamin@users.noreply.github.com>
2021-05-12 11:42:02 +0700

    note on safemath lib

--- SmartWallet/Allowance.sol
+++ SmartWallet/Allowance.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.8.00;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 
 contract Allowance {
-    
+    /*
+    in solidity 0.8.00 overflow and underflow has been addressed by solidity compiler
+    hence, using safemath library is not necessary if the contract is compiled using solidity version 0.8.00 or later
+    */
     using SafeMath for uint;
     
     address public owner;
